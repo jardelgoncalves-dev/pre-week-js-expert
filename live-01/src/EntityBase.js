70bab27192ec299618562b49948c5300b9342f75
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-28 23:38:55 -0300

    feat: add entity base with getters and setters

---
+++ live-01/src/EntityBase.js
@@ -0,0 +1,34 @@
+class EntityBase {
+  #name;
+  #age;
+  #gender;
+
+  constructor({ name, age, gender }) {
+    this.#name = name;
+    this.#age = age;
+    this.#gender = gender;
+  }
+
+  get name() {
+    const preffix = this.#gender === 'male' ? 'Mr.' : 'Ms.'
+    return `${preffix} ${this.#name}`
+  }
+
+  set name(value) {
+    this.#name = value
+  }
+
+  get birthYear() {
+    if(!this.#age) {
+      throw new Error('you must define age first!!!')
+    }
+
+    return new Date().getFullYear() - this.#age;
+  }
+
+  set age(value) {
+    this.#age = value
+  }
+}
+
+module.exports = EntityBase
